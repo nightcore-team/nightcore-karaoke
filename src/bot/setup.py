2f98790272da35f35492943ff9a6567677a52838
monok8i <didkivskiyyura725@gmail.com>
2026-03-03 18:35:20 +0100

    feat(setup): integrate config and system features into bot

--- src/bot/setup.py
+++ src/bot/setup.py
@@ -6,7 +6,9 @@ ENABLED_FEATURES list.
 
 from src.bot.client import NightcoreKaraoke
 from src.bot.events import get_cogs as get_event_cogs
-from src.bot.features.meta.setup import get_cogs as get_meta_cogs
+from src.bot.features.config import get_cogs as get_config_cogs
+from src.bot.features.meta import get_cogs as get_meta_cogs
+from src.bot.features.system import get_cogs as get_system_cogs
 from src.bot.tasks import get_cogs as get_task_cogs
 from src.infra.db.uow import UnitOfWork
 
@@ -20,6 +22,8 @@ def create_bot(uow: UnitOfWork) -> NightcoreKaraoke:
     # To disable a feature, comment it out or set to False
     enabled_features = {
         "meta": True,  # Core commands (ping, info, etc.)
+        "config": True,
+        "system": True,  # System configuration commands (access, etc.)
         # Add more features here as you create them
         # "moderation": True,
         # "music": True,
@@ -44,6 +48,12 @@ def create_bot(uow: UnitOfWork) -> NightcoreKaraoke:
     # Load feature cogs
     if enabled_features.get("meta", False):
         cog_modules.extend(get_meta_cogs())
+
+    if enabled_features.get("config", False):
+        cog_modules.extend(get_config_cogs())
+
+    if enabled_features.get("system", False):
+        cog_modules.extend(get_system_cogs())
 
     # Add more features here
     # if enabled_features.get("moderation", False):
