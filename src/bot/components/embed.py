0876359ce0b576139e714169014cf0dae15724a2
monok8i <didkivskiyyura725@gmail.com>
2026-03-03 18:35:19 +0100

    feat(components): add NoOptionsSuppliedEmbed component

---
+++ src/bot/components/embed.py
@@ -0,0 +1,21 @@
+"""Embed components."""
+
+from typing import TYPE_CHECKING
+
+from discord import Color
+from discord.embeds import Embed
+
+if TYPE_CHECKING:
+    from src.bot.client import NightcoreKaraoke
+
+
+class NoOptionsSuppliedEmbed(Embed):
+    def __init__(self, bot: "NightcoreKaraoke") -> None:
+        super().__init__(
+            title="Не предоставлены параметры",
+            description="Для этой команды не было предоставлено никаких параметров.",  # noqa: E501
+            color=Color.yellow(),
+        )
+        self.set_footer(
+            text=bot.user.name, icon_url=bot.user.display_avatar.url
+        )
