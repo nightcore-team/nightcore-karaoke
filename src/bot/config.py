87e8445c2e0541db77e919605531f8fbf7d5caec
monok8i <didkivskiyyura725@gmail.com>
2026-03-03 18:35:19 +0100

    feat(config): add developer IDs configuration

--- src/bot/config.py
+++ src/bot/config.py
@@ -1,5 +1,7 @@
 """Defines the Config class for bot environment settings."""
 
+from typing import Final
+
 from src.config.env import BaseEnvConfig
 
 
@@ -7,3 +9,7 @@ class Config(BaseEnvConfig):
     """Configuration class for bot environment settings."""
 
     BOT_TOKEN: str
+    DEVELOPERS_IDS: Final[list[int]] = [
+        566255833684508672,
+        1280700292530176131,
+    ]
